a6711e5c9ea81e2a93764cd5798b01f5718d4b43
troynj <troynjohnson6@gmail.com>
2023-03-03 18:19:58 -0800

    added home

--- src/components/tabs/Home.js
+++ src/components/tabs/Home.js
@@ -2,20 +2,12 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+<section id="snapshot">
+        <img id="portrait" src="/assets/images/portrait.png" />
+        <article>
+          <h2>Troy Johnson</h2>
+          <h3>Full Stack Web Developer</h3>
+        </article>
+      </section>
   );
 }
